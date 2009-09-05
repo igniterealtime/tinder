1e9fcf90fafa22eca7e397647faaa7c2e9317202
Guus der Kinderen <guus.der.kinderen@gmail.com>
2009-09-05 12:53:49 +0000

    Adds tests in preparation of TINDER-24

--- src/test/java/org/xmpp/packet/ResourcePrepTest.java
+++ src/test/java/org/xmpp/packet/ResourcePrepTest.java
@@ -1,7 +1,6 @@
 package org.xmpp.packet;
 
 import static org.junit.Assert.assertEquals;
-import gnu.inet.encoding.StringprepException;
 
 import org.junit.Test;
 
@@ -14,7 +13,7 @@ public class ResourcePrepTest {
 
 	/**
 	 * Basic test that verifies that a string that shouldn't be modified by
-	 * resourceprepping gets prepped without a problem.
+	 * resource-prepping gets prepped without a problem.
 	 */
 	@Test
 	public void testValidString() throws Exception {
@@ -27,11 +26,27 @@ public class ResourcePrepTest {
 		// verify
 		assertEquals(resource, result);
 	}
-	
+
 	/**
-	 * Verifies that an input value bigger than 1023 bytes will cause an exception to be thrown.
+	 * Checks that resource-prepping is case sensitive.
 	 */
-	@Test(expected=IllegalArgumentException.class)
+	@Test
+	public void testCaseSensitivity() throws Exception {
+		// setup
+		final String resource = "rEsOuRcE";
+
+		// do magic
+		final String result = JID.resourceprep(resource);
+
+		// verify
+		assertEquals(resource, result);
+	}
+
+	/**
+	 * Verifies that an input value bigger than 1023 bytes will cause an
+	 * exception to be thrown.
+	 */
+	@Test(expected = IllegalArgumentException.class)
 	public void testToLong() throws Exception {
 		// setup
 		final StringBuilder builder = new StringBuilder();
@@ -40,8 +55,45 @@ public class ResourcePrepTest {
 		}
 		builder.append('a');
 		final String toBig = builder.toString();
-		
+
 		// do magic / verify
 		JID.resourceprep(toBig);
+	}
+
+	/**
+	 * Verifies that Stringprep mapping is correctly executed. This test uses a
+	 * 'word joiner' character, which is listed on the B1 table of Stringprep.
+	 * Characters on this table must be mapped in resource strings, according to
+	 * RFC 3920. This specific character should be mapped to nothing.
+	 */
+	@Test
+	public void testMapping() throws Exception {
+		// setup;
+		final String input = "word\u2060joiner";
+
+		// do magic
+		final String result = JID.resourceprep(input);
+
+		// verify
+		assertEquals("wordjoiner", result);
+	}
+
+	/**
+	 * Checks cache usage, by making sure that a subsequent request returns the
+	 * stringprepped answer, not the input data. Input data often equals the
+	 * prepped answer, which allows a bug like this to slip by easily.
+	 */
+	@Test
+	public void testCachedResult() throws Exception {
+		// setup;
+		final String input = "bword\u2060joiner";
+
+		// do magic
+		final String result1 = JID.resourceprep(input);
+		final String result2 = JID.resourceprep(input);
+
+		// verify
+		assertEquals("bwordjoiner", result1);
+		assertEquals(result1, result2);
 	}
 }
