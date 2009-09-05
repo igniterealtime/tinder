d28d9708b65c048380d7eb5b019717e1bdd6f11b
Guus der Kinderen <guus.der.kinderen@gmail.com>
2009-09-05 13:18:06 +0000

    Tests that expose TINDER-29.

---
+++ src/test/java/org/xmpp/packet/JIDCachedBareAndFullJID.java
@@ -0,0 +1,137 @@
+package org.xmpp.packet;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+/**
+ * These tests check for a bug identified as TINDER-29.
+ * 
+ * The implementation of {@link JID} caches a bare and full JID after a
+ * constructor has been used. These tests verify that these cached values are
+ * correct.
+ * 
+ * The full JID is exposed through the {@link JID#toString()} method. The bare
+ * JID is exposed trough {@link JID#toBareJID()}.
+ * 
+ * @author Guus der Kinderen, guus.der.kinderen@gmail.com
+ * @see <a href="http://www.igniterealtime.org/issues/browse/TINDER-29">Tinder
+ *      bugtracker: TINDER-29</a>
+ */
+public class JIDCachedBareAndFullJID {
+
+	/**
+	 * Verifies that Stringprep mapping results are correctly stored in the
+	 * cache. This test uses a 'word joiner' character, which is listed on the
+	 * B1 table of Stringprep. Characters on this table must be mapped in
+	 * resource strings, according to RFC 3920. This specific character should
+	 * be mapped to nothing. The cached value should not contain this character.
+	 */
+	@Test
+	public void testCachedFullJIDisNodePrepped() throws Exception {
+
+		// setup
+		final String node = "word\u2060joiner";
+		final String domain = "domain";
+		final String resource = "resource";
+
+		// do magic
+		final JID result = new JID(node, domain, resource);
+		final String fullJID = result.toString();
+
+		// verify
+		assertEquals("wordjoiner" + "@" + domain + "/" + resource, fullJID);
+	}
+
+	/**
+	 * Verifies that Stringprep mapping results are correctly stored in the
+	 * cache. This test uses a 'word joiner' character, which is listed on the
+	 * B1 table of Stringprep. Characters on this table must be mapped in
+	 * resource strings, according to RFC 3920. This specific character should
+	 * be mapped to nothing. The cached value should not contain this character.
+	 */
+	@Test
+	public void testCachedFullJIDisDomainPrepped() throws Exception {
+
+		// setup
+		final String node = "node";
+		final String domain = "word\u2060joiner";
+		final String resource = "resource";
+
+		// do magic
+		final JID result = new JID(node, domain, resource);
+		final String fullJID = result.toString();
+
+		// verify
+		assertEquals(node + "@" + "wordjoiner" + "/" + resource, fullJID);
+	}
+
+	/**
+	 * Verifies that Stringprep mapping results are correctly stored in the
+	 * cache. This test uses a 'word joiner' character, which is listed on the
+	 * B1 table of Stringprep. Characters on this table must be mapped in
+	 * resource strings, according to RFC 3920. This specific character should
+	 * be mapped to nothing. The cached value should not contain this character.
+	 */
+	@Test
+	public void testCachedFullJIDisResourcePrepped() throws Exception {
+
+		// setup
+		final String node = "node";
+		final String domain = "domain";
+		final String resource = "word\u2060joiner";
+
+		// do magic
+		final JID result = new JID(node, domain, resource);
+		final String fullJID = result.toString();
+
+		// verify
+		assertEquals(node + "@" + domain + "/" + "wordjoiner", fullJID);
+	}
+
+	/**
+	 * Verifies that Stringprep mapping results are correctly stored in the
+	 * cache. This test uses a 'word joiner' character, which is listed on the
+	 * B1 table of Stringprep. Characters on this table must be mapped in
+	 * resource strings, according to RFC 3920. This specific character should
+	 * be mapped to nothing. The cached value should not contain this character.
+	 */
+	@Test
+	public void testCachedBareJIDisNodePrepped() throws Exception {
+
+		// setup
+		final String node = "word\u2060joiner";
+		final String domain = "domain";
+		final String resource = "resource";
+
+		// do magic
+		final JID result = new JID(node, domain, resource);
+		final String bareJID = result.toBareJID();
+
+		// verify
+		assertEquals("wordjoiner" + "@" + domain, bareJID);
+	}
+
+	/**
+	 * Verifies that Stringprep mapping results are correctly stored in the
+	 * cache. This test uses a 'word joiner' character, which is listed on the
+	 * B1 table of Stringprep. Characters on this table must be mapped in
+	 * resource strings, according to RFC 3920. This specific character should
+	 * be mapped to nothing. The cached value should not contain this character.
+	 */
+	@Test
+	public void testCachedBareJIDisDomainPrepped() throws Exception {
+
+		// setup
+		final String node = "node";
+		final String domain = "word\u2060joiner";
+		final String resource = "resource";
+
+		// do magic
+		final JID result = new JID(node, domain, resource);
+		final String bareJID = result.toBareJID();
+
+		// verify
+		assertEquals(node + "@" + "wordjoiner", bareJID);
+	}
+}
