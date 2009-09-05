88a29ec5ee61678a03e28701bc2936241143e942
Guus der Kinderen <guus.der.kinderen@gmail.com>
2009-09-05 19:46:26 +0000

    Additional tests that verify code introduced as part of TINDER-24.

---
+++ src/test/java/org/xmpp/packet/DomainPrepTest.java
@@ -0,0 +1,99 @@
+package org.xmpp.packet;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+/**
+ * Verifies {@link JID#domainprep(String)}.
+ * 
+ * @author Guus der Kinderen, guus.der.kinderen@gmail.com
+ */
+public class DomainPrepTest {
+
+	/**
+	 * Basic test that verifies that a string that shouldn't be modified by
+	 * domain-prepping gets prepped without a problem.
+	 */
+	@Test
+	public void testValidString() throws Exception {
+		// setup
+		final String domain = "domain";
+
+		// do magic
+		final String result = JID.domainprep(domain);
+
+		// verify
+		assertEquals(domain, result);
+	}
+
+	/**
+	 * Checks that domain-prepping is case insensitive.
+	 */
+	@Test
+	public void testCaseSensitivity() throws Exception {
+		// setup
+		final String domain = "dOmAiN";
+
+		// do magic
+		final String result = JID.domainprep(domain);
+
+		// verify
+		assertEquals(domain.toLowerCase(), result);
+	}
+
+	/**
+	 * Verifies that an input value bigger than 1023 bytes will cause an
+	 * exception to be thrown.
+	 */
+	@Test(expected = IllegalArgumentException.class)
+	public void testToLong() throws Exception {
+		// setup
+		final StringBuilder builder = new StringBuilder();
+		for (int i = 0; i + 1 < 1023; i += 2) {
+			builder.append('a');
+		}
+		builder.append('a');
+		final String toBig = builder.toString();
+
+		// do magic / verify
+		JID.domainprep(toBig);
+	}
+
+	/**
+	 * Verifies that Stringprep mapping is correctly executed. This test uses a
+	 * 'word joiner' character, which is listed on the B1 table of Stringprep.
+	 * Characters on this table must be mapped in resource strings, according to
+	 * RFC 3920. This specific character should be mapped to nothing.
+	 */
+	@Test
+	public void testMapping() throws Exception {
+		// setup;
+		final String input = "word\u2060joiner";
+
+		// do magic
+		final String result = JID.domainprep(input);
+
+		// verify
+		assertEquals("wordjoiner", result);
+	}
+
+	/**
+	 * Checks cache usage, by making sure that a subsequent request returns the
+	 * stringprepped answer, not the input data. Input data often equals the
+	 * prepped answer, which allows a bug like this to slip by easily.
+	 */
+	@Test
+	public void testCachedResult() throws Exception {
+		// setup;
+		final String input = "bword\u2060joiner";
+
+		// do magic
+		final String result1 = JID.domainprep(input);
+		final String result2 = JID.domainprep(input);
+
+		// verify
+		assertEquals("bwordjoiner", result1);
+		assertEquals(result1, result2);
+	}
+}
---
+++ src/test/java/org/xmpp/packet/NodePrepTest.java
@@ -0,0 +1,99 @@
+package org.xmpp.packet;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+/**
+ * Verifies {@link JID#nodeprep(String)}.
+ * 
+ * @author Guus der Kinderen, guus.der.kinderen@gmail.com
+ */
+public class NodePrepTest {
+
+	/**
+	 * Basic test that verifies that a string that shouldn't be modified by
+	 * node-prepping gets prepped without a problem.
+	 */
+	@Test
+	public void testValidString() throws Exception {
+		// setup
+		final String node = "node";
+
+		// do magic
+		final String result = JID.nodeprep(node);
+
+		// verify
+		assertEquals(node, result);
+	}
+
+	/**
+	 * Checks that node-prepping is case insensitive.
+	 */
+	@Test
+	public void testCaseSensitivity() throws Exception {
+		// setup
+		final String node = "nOdE";
+
+		// do magic
+		final String result = JID.nodeprep(node);
+
+		// verify
+		assertEquals(node.toLowerCase(), result);
+	}
+
+	/**
+	 * Verifies that an input value bigger than 1023 bytes will cause an
+	 * exception to be thrown.
+	 */
+	@Test(expected = IllegalArgumentException.class)
+	public void testToLong() throws Exception {
+		// setup
+		final StringBuilder builder = new StringBuilder();
+		for (int i = 0; i + 1 < 1023; i += 2) {
+			builder.append('a');
+		}
+		builder.append('a');
+		final String toBig = builder.toString();
+
+		// do magic / verify
+		JID.nodeprep(toBig);
+	}
+
+	/**
+	 * Verifies that Stringprep mapping is correctly executed. This test uses a
+	 * 'word joiner' character, which is listed on the B1 table of Stringprep.
+	 * Characters on this table must be mapped in resource strings, according to
+	 * RFC 3920. This specific character should be mapped to nothing.
+	 */
+	@Test
+	public void testMapping() throws Exception {
+		// setup;
+		final String input = "word\u2060joiner";
+
+		// do magic
+		final String result = JID.nodeprep(input);
+
+		// verify
+		assertEquals("wordjoiner", result);
+	}
+
+	/**
+	 * Checks cache usage, by making sure that a subsequent request returns the
+	 * stringprepped answer, not the input data. Input data often equals the
+	 * prepped answer, which allows a bug like this to slip by easily.
+	 */
+	@Test
+	public void testCachedResult() throws Exception {
+		// setup;
+		final String input = "bword\u2060joiner";
+
+		// do magic
+		final String result1 = JID.nodeprep(input);
+		final String result2 = JID.nodeprep(input);
+
+		// verify
+		assertEquals("bwordjoiner", result1);
+		assertEquals(result1, result2);
+	}
+}
